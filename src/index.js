4bd88fa0b9f205c39668d3b6fa90ea4da6625a9f
Alexandru Cheltuitor <a.cheltuitor.sashkaz@gmail.com>
2018-10-07 15:02:12 +0200

    Configured the files so it uploads to correct place

--- src/index.js
+++ src/index.js
@@ -1,1 +1,6 @@
 console.log('This is a new test');
+console.log('This is a new test');
+console.log('This is a new test');
+
+var x = 7;
+console.log(x);
--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,40 @@
-var path = require('path');
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 module.exports = {
     mode: 'development',
     entry: {
-        index: './src/index.js'
+        index: './src/index.js',
+        contact: './src/contact.js'
     },
+    plugins: [
+        new CleanWebpackPlugin(['dist']),
+        new HtmlWebpackPlugin({
+            title: 'Main Page',
+            filename: 'index.html',
+            hash: true,
+            excludeChunks: ['contact'],
+        }),
+        new HtmlWebpackPlugin({
+            title: 'Contact',
+            filename: 'contact.html',
+            template: './src/contact.html',
+            hash: true,
+            chunks: ['contact'],
+        }),
+    ],
+    devServer: {
+        contentBase: './dist' 
+    },    
+    devtool: 'inline-source-map',
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: '[name].bundle.js'
+    },
+    optimization: {
+        splitChunks: {
+            chunks: 'all'
+        }
     }
 }
