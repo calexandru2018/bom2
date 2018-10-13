e851ef3d46edcc80e0c1a3cb9f03627ff22f6667
calexandru2018 <calexandru2018@mail.com>
2018-10-13 22:46:09 +0200

    fixed parallax effect and nav is working

--- src/js/index.js
+++ src/js/index.js
@@ -2,8 +2,13 @@
 import 'normalize.css';
 import '../css/main.css';
 
-
-console.log('This is a new test');
-
-var x = 7;
-console.log(x);
+document.querySelectorAll('.nav-toggle').forEach(el => {
+    var mainNav = document.querySelector('.main-nav');
+    el.addEventListener('click', () => {
+        if(mainNav.classList.contains('open') == false){
+            mainNav.classList.add('open');
+        }else{
+            mainNav.classList.remove('open');
+        }
+    });
+});
