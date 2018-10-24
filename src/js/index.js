296d3b8bb32fc4d3b4944764695431f3314872ea
calexandru2018 <calexandru2018@gmail.com>
2018-10-24 22:02:09 +0200

    Added the content to the cards

--- src/js/index.js
+++ src/js/index.js
@@ -38,12 +38,12 @@ scrollButton.addEventListener('click', () => {
         behavior: "smooth"
     });
 });
-/* window.onload = (() => {
+window.onload = (() => {
     parallaxScroll.scroll({
-        top: 5500,
+        top: 1500,
         // behavior: "smooth"
     });
-}); */
+});
 /* function fadeIn(el, time) {
   el.style.opacity = 0;
 
