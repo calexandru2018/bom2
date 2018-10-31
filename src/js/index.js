e2783e564ce368477128a4ded4a951c143e48b79
calexandru2018 <calexandru2018@gmail.com>
2018-11-01 00:03:22 +0100

    Navigation buttons are ready

--- src/js/index.js
+++ src/js/index.js
@@ -10,24 +10,24 @@ import '../img/logo.png';
 
 var parallaxScroll = document.querySelector('.parallax-wrapper');
 var pixelScrollLimit = 450; 
+var mainNavigation = document.querySelector('.main-nav');
 var scrollToTopButton = document.getElementById('scroll-to-top');
 var showLocatorBtn = document.getElementById('show-locator');
 var scrollToProductsBtn = document.getElementById('scroll-to-products');
 
 /* Nav buttons */
-var scrollToHomeBtn = document.getElementById('scroll-to-home');
-var scrollToConceptBtn = document.getElementById('scroll-to-concept');
-// var scrollToProductBtn = document.getElementById('scroll-to-home');
-var scrollToStoryBtn = document.getElementById('scroll-to-story');
-var scrollToHashtagBtn = document.getElementById('scroll-to-hashtag');
-var scrollToContactsBtn = document.getElementById('scroll-to-contacts');
-var scrollToEventsBtn = document.getElementById('scroll-to-events');
+/* var scrollToHomeBtn = document.getElementById('scroll-home-btn'); */
+var scrollToConceptBtn = document.getElementById('scroll-concept-btn');
+// var scrollToProductBtn = document.getElementById('scroll-home-btn');
+var scrollToStoryBtn = document.getElementById('scroll-story-btn');
+var scrollToHashtagBtn = document.getElementById('scroll-hashtag-btn');
+var scrollToContactsBtn = document.getElementById('scroll-contacts-btn');
+var scrollToEventsBtn = document.getElementById('scroll-events-btn');
 /* Nav buttons */
 
 document.querySelectorAll('.nav-toggle').forEach(el => {
-    var mainNav = document.querySelector('.main-nav');
     el.addEventListener('click', () => {
-        mainNav.classList.toggle('open');
+        mainNavigation.classList.toggle('open');
     });
 });
 parallaxScroll.addEventListener('scroll', () => {
@@ -51,12 +51,48 @@ showLocatorBtn.addEventListener('click', () => {
 });
 scrollToProductsBtn.addEventListener('click', () => {
     var top = document.getElementById('products').getBoundingClientRect();
+    var parent = window.pageYOffset || parallaxScroll.scrollTop || document.body.scrollTop;    
     parallaxScroll.scroll({
-        top: top.top,
+        top: top.top + parent,
+        left: 0,
+        behavior: "smooth"
+    }); 
+});
+/* scrollToHomeBtn.addEventListener('click', (el) => {
+    var element = el.srcElement.dataset.link;  
+    scrollTo(document.getElementById(element));  
+}); */
+scrollToConceptBtn.addEventListener('click', (el) => {
+    var element = el.srcElement.dataset.link;  
+    scrollTo(document.getElementById(element));  
+});
+scrollToStoryBtn.addEventListener('click', (el) => {
+    var element = el.srcElement.dataset.link;  
+    scrollTo(document.getElementById(element));  
+});
+scrollToHashtagBtn.addEventListener('click', (el) => {
+    var element = el.srcElement.dataset.link;  
+    scrollTo(document.getElementById(element));  
+});
+scrollToContactsBtn.addEventListener('click', (el) => {
+    var element = el.srcElement.dataset.link;  
+    scrollTo(document.getElementById(element));  
+});
+scrollToEventsBtn.addEventListener('click', (el) => {
+    var element = el.srcElement.dataset.link;  
+    scrollTo(document.getElementById(element));  
+});
+
+function scrollTo(el){
+    var top = el.getBoundingClientRect();
+    var parent = window.pageYOffset || parallaxScroll.scrollTop || document.body.scrollTop;
+    parallaxScroll.scroll({
+        top: top.top + parent,
         left: 0,
         behavior: "smooth"
     });
-});
+    mainNavigation.classList.toggle('open'); 
+}
 /* window.onload = (() => {
     parallaxScroll.scroll({
         top: 6700,
