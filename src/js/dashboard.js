d060d9597e65bb05cbc4a537c5efdca5b21734a4
Alexandru Cheltuitor <calexandru2018@gmail.com>
2018-11-06 14:21:44 +0100

    Layout with basic functionality is done

--- src/js/dashboard.js
+++ src/js/dashboard.js
@@ -1,2 +1,15 @@
 import 'normalize.css';
 import '../css/dashboard.css';
+
+document.querySelectorAll('.btn-primary').forEach((btn) => {
+    btn.addEventListener('click', () => {
+        const nextEl = btn.nextElementSibling;
+        nextEl.classList.toggle('display-grid'); 
+    });
+});
+document.querySelectorAll('.btn-secondary').forEach((btn) => {
+    btn.addEventListener('click', () => {
+        const nextEl = btn.nextElementSibling;
+        nextEl.classList.toggle('display-grid'); 
+    });
+});
