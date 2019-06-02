eb52f9da329bcd1a654a4836d21159152cb149e0
Alexandru Cheltuitor <calexandru2018@pm.me>
2019-06-02 22:07:43 +0200

    Created a folder structure

---
+++ login.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="pt">
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Login - Bom&sup2;</title>
+<link href="assets/css/vendors~index.css?818e20abaae123fdbd40" rel="stylesheet"><link href="assets/css/login.css?818e20abaae123fdbd40" rel="stylesheet"></head>
+<body>
+    <div class="page-wrapper">
+        <img class="self-center" src="assets/img/logo.png" alt="Imagem do administractor" srcset="">
+        <form action="./dashboard.html" class="login-form">
+            <label for="username">Utilizador</label>
+            <input type="text" name="adminUsername" id="adminUsername">
+            <label for="adminPassword">Password</label>
+            <input type="password" name="adminPassword" id="adminPassword">
+
+            <button type="submit" class="btn btn-primary">Entrar</button>
+            </form>
+    </div>
+<script type="text/javascript" src="assets/js/vendors~index.js?818e20abaae123fdbd40"></script><script type="text/javascript" src="assets/js/login.js?818e20abaae123fdbd40"></script></body>
+</html>
