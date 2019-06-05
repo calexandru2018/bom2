160a2c52a51f81c2ec1c00412a2da737fd7ef222
Alexandru Cheltuitor <calexandru2018@pm.me>
2019-06-05 23:07:53 +0200

    Code working with password hashing

--- admin/models/admin.php
+++ admin/models/admin.php
@@ -1,21 +1,22 @@
 <?php 
     function createAdmin(Array $admin, $dbConn){
+        $pwd = password_hash(mysqli_real_escape_string($dbConn, $admin['pwd']), PASSWORD_BCRYPT);
         $sql = "
         insert into admin (
             name, 
             email, 
-            password,
-            ".(($admin['phoneNumber']) ? 'phone_number':'').") 
+            password
+            ".(($admin['phoneNumber']) ? ', phone_number':'').") 
         values (
             '".$admin['name']."',
             '".$admin['email']."',
-            '".$admin['pwd']."',
-            '".(($admin['phoneNumber']) ? $admin['phoneNumber']:'')."')
+            '".$pwd."'
+            ".(($admin['phoneNumber']) ? ",'".$admin['phoneNumber'].".":'').")
         ";
         $query = $dbConn->query($sql);
         if($dbConn->affected_rows === 1)
             return 1;
         else    
-            return $sql;
+            return mysqli_error($dbConn).$sql;
     }
 ?>
