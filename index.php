02148b78acf891f0ad69cbde0b90a7d153bcd266
Alexandru Cheltuitor <calexandru2018@pm.me>
2019-06-11 12:19:32 +0100

    Made so that languages are identified automtically based on browser, thereafter are based on user choice

--- index.php
+++ index.php
@@ -1,11 +1,29 @@
 <?php 
     session_start();
+    function get_client_language($availableLanguages, $default='en'){
+        if (isset($_SERVER['HTTP_ACCEPT_LANGUAGE'])) {
+            $langs=explode(',',$_SERVER['HTTP_ACCEPT_LANGUAGE']);
+    
+            foreach ($langs as $value){
+                $choice=substr($value,0,2);
+                if(in_array($choice, $availableLanguages)){
+                    return $choice;
+                }
+            }
+        } 
+        return $default;
+    }
+    $availableLanguages = ['pt', 'en'];
     if(!isset($_COOKIE['accept_cookie'])){
         setcookie('accept_cookie', md5(0),  time()+60*60*24*30, "/bom2", '', false); //localhost
         // setcookie('accept_cookie', md5(0),  time()+60*60*24*30, '/', '', true);
     }
-    $LANG = 'pt';
-    if(isset($_GET['lang']) && $_GET['lang']=='en')
+    
+    if(!isset($_GET['lang']))
+        $LANG = get_client_language($availableLanguages, $default='en');
+    elseif(isset($_GET['lang']) && $_GET['lang'] == 'pt')
+        $LANG = 'pt';
+    elseif(isset($_GET['lang']) && $_GET['lang'] == 'en')
         $LANG = 'en';
 
     require('./assets/lang/_'.$LANG.'.php');
